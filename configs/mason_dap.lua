ab59f930fdfb80573e0124100dc0b36e4ec53ad6
Asep Komarudin <68836805+pojokcodeid@users.noreply.github.com>
2023-04-10 18:38:17 +0700

    Update mason_dap.lua

--- configs/mason_dap.lua
+++ configs/mason_dap.lua
@@ -1,13 +1,12 @@
-local sources = {}
-local data_exists, data = pcall(require, "core.config")
-if data_exists then
-  for _, nullls in pairs(data.dap_ensure_installed) do
-    table.insert(sources, nullls)
-  end
-end
 require("mason").setup()
 require("mason-nvim-dap").setup {
-  ensure_installed = sources,
   automatic_setup = true,
+  handlers = {
+    function(config)
+      -- all sources with no handler get passed here
+
+      -- Keep original functionality
+      require("mason-nvim-dap").default_setup(config)
+    end,
+  },
 }
--- require("mason-nvim-dap").setup_handlers {}
