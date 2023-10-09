d9c2efbe6b1ba3f172321485b3d33dbe856f6014
pojok code <pojokcodeid@gmail.com>
2023-10-09 17:26:49 +0700

    update

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -43,8 +43,8 @@ M.nvimtree = {
   --   enable = true,
   -- },
   git = {
-    enable = false, -- false dulu karena muncul error
-    ignore = true, -- true dulu karena muncul error
+    enable = true, -- false dulu karena muncul error
+    ignore = false, -- true dulu karena muncul error
     show_on_dirs = true,
     show_on_open_dirs = true,
     timeout = 200,
