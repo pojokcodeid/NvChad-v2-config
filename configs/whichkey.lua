8934f03a916ae4fee5e22fc7b5a892ce6109a84f
Pojok Code <pojokcodeid@gmail.com>
2024-03-03 07:14:30 +0700

    add: config whichkey border rounded

--- configs/whichkey.lua
+++ configs/whichkey.lua
@@ -64,7 +64,13 @@ local setup = {
   },
 
   window = {
-    border = "none", -- none/single/double/shadow
+    -- border = "none", -- none/single/double/shadow
+    border = "rounded", -- none, single, double, shadow
+    -- border = "none", -- none, single, double, shadow
+    position = "bottom", -- bottom, top
+    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+    winblend = 0,
   },
 
   layout = {
